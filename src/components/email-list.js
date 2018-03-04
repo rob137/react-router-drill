db4d9944308d4cd83c54cc5f194d624350fff4d3
Glitch (react-email-starting-point) <none>
2018-03-04 09:32:42 +0000

    initial commit; added routing by email folder name

--- src/components/email-list.js
+++ src/components/email-list.js
@@ -3,35 +3,38 @@ import {connect} from 'react-redux';
 import './email-list.css';
 
 export function EmailList(props) {
-    const emails = props.emailList.map(email =>
-        <li className="email-list-email">
-            <div className="email-list-email-from">
-                {email.from}
-            </div>
-            <div className="email-list-email-title">
-                {email.title}
-            </div>
-        </li>
-    );
-
+  const emails = props.emailList.map((email, index) => {
     return (
-        <div className="folder">
-            <h2>{props.folderName}</h2>
-            <ul className="email-list">
-                {emails}
-            </ul>
-        </div>
-    );
+			<li className="email-list-email" key={index}>
+    	  <div className="email-list-email-from">
+    	    {email.from}
+    	  </div>
+    	  <div className="email-list-email-title">
+    	    {email.title}
+    	  </div>
+    	</li>
+		)
+  });
+
+  return (
+    <div className="folder">
+      <h2>{props.folderName}</h2>
+      <ul className="email-list">
+        {emails}
+      </ul>
+    </div>
+  );
 }
 
-const mapStateToProps = (state, props) => {
-    const folder = state[props.folderId];
-    return {
-        folderName: folder.name,
-        emailList: Object.keys(folder.emails).map(emailId =>
-            folder.emails[emailId]
-        )
-    }
+
+const mapStateToProps = (state, props) => { 
+  const folder = state[props.folderId];
+	return {
+    folderName: folder.name,
+    emailList: Object.keys(folder.emails).map(emailId =>
+      folder.emails[emailId]
+    )
+  }
 };
 
 export default connect(mapStateToProps)(EmailList);
--- src/components/email.js
+++ src/components/email.js
@@ -2,16 +2,25 @@ import React from 'react';
 import Sidebar from './sidebar';
 import SingleEmail from './single-email';
 import EmailList from './email-list';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './email.css';
 
-export default function Email() {
-    return (
-        <div className="email">
-            <Sidebar />
-            <main>
-                <EmailList folderId="inbox" />
-                {/* <SingleEmail folderId="inbox" emailId="1" /> */}
-            </main>
-        </div>
-    );
+export default class Email extends React.Component {
+	render() {
+		return (
+			<Router>
+				<div className="email">
+					<Sidebar/>
+					<main>
+						<Route exact path='/' render={() => <EmailList folderId="inbox" />} />
+						<Route exact path='/inbox' render={() => <EmailList folderId="inbox" />} />
+						<Route exact path='/spam' render={() => <EmailList folderId="spam" />} />
+
+						{/* <EmailList folderId={this.state.folderId}/> */}
+						{/* <SingleEmail folderId="inbox" emailId="1" /> */}
+					</main>
+				</div>
+			</Router>
+		);
+	}
 }
