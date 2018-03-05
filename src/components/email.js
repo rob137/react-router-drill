52f58065f5813f384a842adb165c5aac04e6357a
Glitch (react-email-starting-point) <none>
2018-03-05 09:57:57 +0000

    add Links to sidebar; add Routes for email lists

--- src/components/email.js
+++ src/components/email.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Sidebar from './sidebar';
 import SingleEmail from './single-email';
 import EmailList from './email-list';
 import './email.css';
 
 export default function Email() {
-    return (
-        <div className="email">
-            <Sidebar />
-            <main>
-                <EmailList folderId="inbox" />
-                {/* <SingleEmail folderId="inbox" emailId="1" /> */}
-            </main>
-        </div>
-    );
+  return (
+		<Router>
+			<div className="email">
+    		<Sidebar />
+    		<main>
+    			<Route exact path="(/|/inbox)" render={() => <EmailList folderId="inbox" />} />
+					<Route exact path='/spam' render={() => <EmailList folderId="spam" />} />
+					
+    			{/* <SingleEmail folderId="inbox" emailId="1" /> */}
+    		</main>
+  		</div>
+		</Router>
+);
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import './sidebar.css';
 
 export function Sidebar(props) {
-    const folders = props.folderList.map(folder =>
-        <li key={folder.id} className="folder-menu-list-item">
-            {folder.name}
-        </li>
-    );
+	const folders = props.folderList.map(folder =>
+		<li key={folder.id} className="folder-menu-list-item">
+			<Link to={`/${folder.name}`}> {folder.name} </Link>
+		</li>
+	);
 
-    return (
-        <div className="sidebar sidebar-left">
-            <nav className="folder-menu">
-                <ul className="folder-menu-list">
-                    {folders}
-                </ul>
-            </nav>
-        </div>
-    );
+	return (
+		<div className="sidebar sidebar-left">
+			<nav className="folder-menu">
+				<ul className="folder-menu-list">
+					{folders}
+				</ul>
+			</nav>
+		</div>
+	);
 }
 
 const mapStateToProps = state => ({
