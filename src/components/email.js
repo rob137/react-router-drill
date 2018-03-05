e8305fb5574ee963694c5414698adc133b8383e6
Glitch (react-email-starting-point) <none>
2018-03-05 10:30:19 +0000

    for Maxim to review

--- src/components/email.js
+++ src/components/email.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Sidebar from './sidebar';
 import SingleEmail from './single-email';
 import EmailList from './email-list';
